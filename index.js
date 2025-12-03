119bb7f14d1e8a8a07c64896544fe593aaed8db1
George-Okumu <gokumu12@gmail.com>
2025-12-03 09:40:29 +0300

    simple solution

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
 
+let userInfo = {username: '', password: ''};
+
+function createLoginTracker(userInfo){
+  let attemptCount = 0;
+
+  return (passwordAttempt) => {
+    if(passwordAttempt === userInfo.password){
+      return "Login successful";
+    }
+
+    if(passwordAttempt !== userInfo.password){
+      attemptCount++;
+
+      if(attemptCount <= 3){
+        return `Attempt ${attemptCount}: Login failed`;
+      }
+
+      if(attemptCount > 3){
+        return "Account locked due to too many failed login attempts";
+      }
+    }
+  }
+
+}
 
 
 module.exports = {
   ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
 };
+
+
